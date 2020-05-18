f9aca900a5ce29bc4f9672a5b187a4276a37f872
jakubarendac <jakub.arendac105@gmail.com>
2020-05-18 23:50:28 +0200

    removed unused constants

--- constants/MinutiaeExtractor_constants.py
+++ constants/MinutiaeExtractor_constants.py
@@ -1,3 +1,3 @@
-COARSE_NET_PATH = './MinutiaeNet/Models/CoarseNet.h5'
-FINE_NET_PATH = './MinutiaeNet/Models/FineNet.h5'
-CLASSIFY_NET_PATH = '/home/jakub/projects/minutiae-extractor/ClassifyNet/Models/ClassifyNet_4_classes.h5'
+COARSE_NET_PATH = ''
+FINE_NET_PATH = ''
+CLASSIFY_NET_PATH = ''
